adc674699ee7c7e3bb5c55db5dbfd60e6ed20bd6
Oscar Arámbula <peteroso876@gmail.com>
2024-09-10 22:55:10 -0600

    Configuring the LLM

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from templates import ThePrimeagen
 
 llm = OpenAI(
     openai_api_key=config.OPENAI_API_KEY,
+    model="gpt-3.5-turbo-instruct",
+    temperature=0
 )
 
 if __name__ == '__main__':
-    response = llm.invoke(ThePrimeagen.format(advice="how to be a good at swimming"))
+    response = llm.invoke(ThePrimeagen.format(advice="Which programming language should I learn?"))
 
     print(response)
