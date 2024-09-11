4f5bd05b3f147693ff10f5aae56a855c48a94c00
Oscar Arámbula <peteroso876@gmail.com>
2024-09-10 23:34:23 -0600

    Wrapping in a Chain

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
 import config
 
 chat_llm = ChatOpenAI(
     openai_api_key=config.OPENAI_API_KEY,
 )
 
-instructions = SystemMessage(content="""
-You are a surfer dude, having a conversation about the surf conditions on the beach.
-Respond using surfer slang.
-""")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a surfer dude, having a conversation about the surf conditions on the beach. Respond using surfer slang.",
+        ),
+        (
+            "human",
+            "{question}"
+        ),
+    ]
+)
 
-question = HumanMessage(content="What is the weather like?")
+chat_chain = prompt | chat_llm | StrOutputParser()
 
 if __name__ == '__main__':
-    response = chat_llm.invoke([
-        instructions,
-        question,
-    ])
+    response = chat_chain.invoke({"question": "What's the surf like today?"})
 
-    print(response.content)
+    print(response)
