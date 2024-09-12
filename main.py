3b2852f5e38401658fa9c1af84011ab008218c64
Oscar Arámbula <peteroso876@gmail.com>
2024-09-11 22:56:10 -0600

    Querying a vector index

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from langchain_community.tools import YouTubeSearchTool
 from langchain_community.chat_message_histories import Neo4jChatMessageHistory
 from langchain_community.graphs import Neo4jGraph
 from uuid import uuid4
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Neo4jVector
 from config import NEO4J_URL, NEO4J_USERNAME, NEO4J_PASSWORD, OPENAI_API_KEY
 
 SESSION_ID = str(uuid4())
@@ -16,10 +18,22 @@ print(f"Session ID: {SESSION_ID}")
 
 llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
 
+embedding_provider = OpenAIEmbeddings(
+    openai_api_key=OPENAI_API_KEY
+)
+
 graph = Neo4jGraph(
     url=NEO4J_URL,
     username=NEO4J_USERNAME,
     password=NEO4J_PASSWORD
+)
+
+movie_plot_vector = Neo4jVector.from_existing_index(
+    embedding_provider,
+    graph=graph,
+    index_name="moviePlots",
+    embedding_node_property="plotEmbedding",
+    text_node_property="plot",
 )
 
 prompt = ChatPromptTemplate.from_messages(
@@ -71,14 +85,6 @@ chat_agent = RunnableWithMessageHistory(
 )
 
 if __name__ == "__main__":
-    while True:
-        q = input("> ")
-
-        response = chat_agent.invoke(
-            {
-                "input": q
-            },
-            {"configurable": {"session_id": SESSION_ID}},
-        )
-
-        print(response["output"])
+    result = movie_plot_vector.similarity_search("A movie where a man forgets he killed his wife.")
+    for doc in result:
+        print(doc.metadata["title"], "-", doc.page_content)
