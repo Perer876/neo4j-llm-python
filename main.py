8dba3039a0ce34b7bf41fe10d8d2b1b247495835
Oscar Arámbula <peteroso876@gmail.com>
2024-09-11 22:40:02 -0600

    Agents

--- main.py
+++ main.py
@@ -1,64 +1,84 @@
-from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain.tools import Tool
+from langchain import hub
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.schema import StrOutputParser
+from langchain_community.tools import YouTubeSearchTool
 from langchain_community.chat_message_histories import Neo4jChatMessageHistory
-from db import graph
-from config import OPENAI_API_KEY
+from langchain_community.graphs import Neo4jGraph
 from uuid import uuid4
+from config import NEO4J_URL, NEO4J_USERNAME, NEO4J_PASSWORD, OPENAI_API_KEY
 
 SESSION_ID = str(uuid4())
 print(f"Session ID: {SESSION_ID}")
 
-def get_memory(session_id):
-    return Neo4jChatMessageHistory(session_id=session_id, graph=graph)
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
 
-chat_llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
+graph = Neo4jGraph(
+    url=NEO4J_URL,
+    username=NEO4J_USERNAME,
+    password=NEO4J_PASSWORD
+)
 
 prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a surfer dude, having a conversation about the surf conditions on the beach. Respond using surfer slang.",
+            "You are a movie expert. You find movies from a genre or plot.",
         ),
-        ( "system", "When you do not know a beach condition, you should apologize" ),
-        ( "system", "{context}" ),
-        MessagesPlaceholder(variable_name="chat_history"),
-        ( "human", "{question}" ),
+        ("human", "{input}"),
     ]
 )
 
-chat_chain = prompt | chat_llm | StrOutputParser()
+movie_chat = prompt | llm | StrOutputParser()
 
-current_weather = """
-{
-    "surf": [
-        {"beach": "Fistral", "conditions": "6ft waves and offshore winds"},
-        {"beach": "Polzeath", "conditions": "Flat and calm"},
-        {"beach": "Watergate Bay", "conditions": "3ft waves and onshore winds"}
-    ]
-}"""
+youtube = YouTubeSearchTool()
 
-chat_with_message_history = RunnableWithMessageHistory(
-    chat_chain,
+
+def get_memory(session_id):
+    return Neo4jChatMessageHistory(session_id=session_id, graph=graph)
+
+
+def call_trailer_search(question):
+    question = question.replace(",", " ")
+    return youtube.run(question)
+
+
+tools = [
+    Tool.from_function(
+        name="Movie Chat",
+        description="For when you need to chat about movies. The question will be a string. Return a string.",
+        func=movie_chat.invoke,
+    ),
+    Tool.from_function(
+        name="Movie Trailer Search",
+        description="Use when needing to find a movie trailer. The question will include the word trailer. Return a link to a YouTube video.",
+        func=call_trailer_search,
+    ),
+]
+
+agent_prompt = hub.pull("hwchase17/react-chat")
+agent = create_react_agent(llm, tools, agent_prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+chat_agent = RunnableWithMessageHistory(
+    agent_executor,
     get_memory,
-    input_messages_key="question",
+    input_messages_key="input",
     history_messages_key="chat_history",
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while True:
-        question = input("> ")
+        q = input("> ")
 
-        response = chat_with_message_history.invoke(
+        response = chat_agent.invoke(
             {
-                "context": current_weather,
-                "question": question,
-
+                "input": q
             },
-            config={
-                "configurable": {"session_id": SESSION_ID}
-            }
+            {"configurable": {"session_id": SESSION_ID}},
         )
 
-        print(response)
+        print(response["output"])
